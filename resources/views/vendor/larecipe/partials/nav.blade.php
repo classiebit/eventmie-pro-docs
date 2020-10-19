b1e5868eebb23f8de97edb8d8bbabada14ebeb5f
Deepak Panwar <dp.classiebit@gmail.com>
2020-10-19 12:10:23 +0530

    update price

--- resources/views/vendor/larecipe/partials/nav.blade.php
+++ resources/views/vendor/larecipe/partials/nav.blade.php
@@ -28,7 +28,7 @@
             @endif
 
             <larecipe-button tag="a" href="https://classiebit.com/eventmie-pro" target="__blank" type="success" class="mx-2 px-4">
-                <i class="fas fa-shopping-cart"></i> Purchase Pro $59 <span style="text-decoration: line-through;color: #f00;">$109</span>
+                <i class="fas fa-shopping-cart"></i> Purchase Pro $69 <span style="text-decoration: line-through;color: #f00;">$109</span>
             </larecipe-button>
 
             {{-- versions dropdown --}}
